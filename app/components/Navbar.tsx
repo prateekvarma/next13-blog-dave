586408567c615fc9e257f3c7d2ed34f11d010a59
prateekvarma <prtk.varma@gmail.com>
2023-07-08 08:59:37 -0600

    Added icons to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
@@ -12,6 +13,32 @@ export default function Navbar() {
             Prateek Varma
           </Link>
         </h1>
+        <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl'>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://www.youtube.com/@codehowl'
+          >
+            <FaYoutube />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://codwhowl.com/'
+          >
+            <FaLaptop />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://github.com/prateekvarma'
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            className='text-white/90 hover:text-white'
+            href='https://twitter.com/prateekvarma'
+          >
+            <FaTwitter />
+          </Link>
+        </div>
       </div>
     </nav>
   );
