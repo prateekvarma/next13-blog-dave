6a215c43e8d930738414ba9271fe66b51498964d
prateekvarma <prtk.varma@gmail.com>
2023-07-11 17:09:12 -0600

    Route handler for any URL params

--- app/api/echo/route.ts
+++ app/api/echo/route.ts
@@ -1,10 +1,19 @@
 import { NextResponse } from "next/server";
 
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const name = searchParams.get('name');
+//   const instrument = searchParams.get('instrument');
+
+//   // expects URL GET for something like http://localhost:3000/api/echo?name=prateek&instrument=guitar
+//   return NextResponse.json({ name, instrument }); 
+// }
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const name = searchParams.get('name');
-  const instrument = searchParams.get('instrument');
 
-  // expects URL GET for something like http://localhost:3000/api/echo?name=prateek&instrument=guitar
-  return NextResponse.json({ name, instrument }); 
+  const obj = Object.fromEntries(searchParams.entries());
+  // this returns *any* URL params provided
+
+  return NextResponse.json(obj); 
 }
