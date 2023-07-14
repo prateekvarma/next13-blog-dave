22df8d91028801b4acfae8f899ecbfe62aeabb8a
prateekvarma <prtk.varma@gmail.com>
2023-07-14 10:48:42 -0600

    DELETE request for route handler

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,9 +1,23 @@
 import { NextResponse } from 'next/server';
 
 const DATA_SOURCE_URL = 'http://jsonplaceholder.typicode.com/todos';
+const API_KEY: string = process.env.DATA_API_KEY as string;
 
 export async function GET() {
   const res = await fetch(DATA_SOURCE_URL);
   const todos: Todo[] = await res.json();
   return NextResponse.json(todos);
+}
+
+export async function DELETE(request: Request) {
+  const { id }: Partial<Todo> = await request.json();
+  if (!id) return NextResponse.json({ message: 'Todo Id is required' });
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'Application/json',
+      'API-Key': API_KEY,
+    },
+  });
+  return NextResponse.json({ message: `Todo ${id} deleted` });
 }
