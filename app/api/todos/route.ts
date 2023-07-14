423f6c46cdb271000251e403f6c5069f88fe7fd7
prateekvarma <prtk.varma@gmail.com>
2023-07-14 11:08:40 -0600

    PUT request for Route Handler

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -44,3 +44,26 @@ export async function POST(request: Request) {
 
   return NextResponse.json(newTodo);
 }
+
+export async function PUT(request: Request) {
+  const { userId, id, title, completed }: Todo = await request.json();
+
+  if (!userId || !id || !title || typeof(completed) !== 'boolean')
+    return NextResponse.json({ message: 'userId, title, id & completed fields are required' });
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'Application/json',
+      'API-Key': API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed
+    }),
+  });
+
+  const updatedTodo: Todo = await res.json();
+
+  return NextResponse.json(updatedTodo);
+}
