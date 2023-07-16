e5e91c81ee6e530b58eb5599daef15ecf63731c0
prateekvarma <prtk.varma@gmail.com>
2023-07-16 13:46:22 -0600

    Middleware initial setup

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+  console.log('Middleware');
+  console.log(request.method);
+  console.log(request.url);
+
+  const origin = request.headers.get('origin');
+  console.log(origin);
+
+  return NextResponse.next();
+  
+}
