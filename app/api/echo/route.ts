6c37fdbb113a803b828d60d7f7d6afdd689e55c2
prateekvarma <prtk.varma@gmail.com>
2023-07-16 14:20:50 -0600

    Installed limiter package, and used it to configure Rate Limiting in the echo route

--- app/api/echo/route.ts
+++ app/api/echo/route.ts
@@ -1,4 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { limiter } from '../config/limiter';
 
 // export async function GET(request: Request) {
 //   const { searchParams } = new URL(request.url);
@@ -6,14 +7,29 @@ import { NextResponse } from "next/server";
 //   const instrument = searchParams.get('instrument');
 
 //   // expects URL GET for something like http://localhost:3000/api/echo?name=prateek&instrument=guitar
-//   return NextResponse.json({ name, instrument }); 
+//   return NextResponse.json({ name, instrument });
 // }
 
 export async function GET(request: Request) {
+  const origin = request.headers.get('origin');
+
+  const remaining = await limiter.removeTokens(1);
+  console.log('Remaning tokens: ', remaining);
+  if (remaining < 0) {
+    return NextResponse.json(null, {
+      status: 429,
+      statusText: 'Too many requests',
+      headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   const { searchParams } = new URL(request.url);
 
   const obj = Object.fromEntries(searchParams.entries());
   // this returns *any* URL params provided
 
-  return NextResponse.json(obj); 
+  return NextResponse.json(obj);
 }
