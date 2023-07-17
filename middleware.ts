b711bf8c76c153a69aede0d4a54a92a18e3f980e
prateekvarma <prtk.varma@gmail.com>
2023-07-17 15:15:41 -0600

    Added middleware to check origin(CORS)

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
 import { NextResponse } from 'next/server';
 
+const allowedOrigins =
+  process.env.NODE_ENV === 'production'
+    ? ['https://www.yoursite.com', 'https://yoursite.com']
+    : ['http://localhost:3000', 'https://google.com'];
+
 export function middleware(request: Request) {
+  const origin = request.headers.get('origin');
+  console.log(origin);
+
+  //below, add || !origin - if your want the origin to includes in the allowedOrigins, and if origin is absent like the postman tool. Check dave 4:39:10
+  if(origin && !allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: 'Bad Request',
+      headers: {
+        'Context-Type': 'text/plain',
+      }
+    })
+  }
+  
   console.log('Middleware');
   console.log(request.method);
   console.log(request.url);
 
-  const origin = request.headers.get('origin');
-  console.log(origin);
 
   return NextResponse.next();
 }
